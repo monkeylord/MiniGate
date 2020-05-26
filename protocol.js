3c3aeb06ad1e2c836312e1de49cfa559ac0021e4
Karl Semich <0xloem@gmail.com>
2020-05-26 12:10:14 -0400

    genesis bugfix

--- protocol.js
+++ protocol.js
@@ -10,14 +10,14 @@ Protocol.resolveD = function (tx){
     var script = DataOut.script
     // Malformed
     if(script.chunks.length<6)return null
-    if(script.chunks[1].buf == new Buffer('19iG3WTYSsbyos3uJ733yK4zEioi1FesNU')){
+    if(script.chunks[1].buf && script.chunks[1].buf.toString() === '19iG3WTYSsbyos3uJ733yK4zEioi1FesNU'){
         return {
             key: script.chunks[2].buf.toString('utf-8'),
             value: script.chunks[3].buf.toString('utf-8'),
             type: script.chunks[4].buf.toString(),
             sequence: parseInt(script.chunks[5].buf)||1
         }
-    } else if(script.chunks[2].buf == new Buffer('19iG3WTYSsbyos3uJ733yK4zEioi1FesNU')){
+    } else if(script.chunks[2].buf && script.chunks[2].buf.toString() === '19iG3WTYSsbyos3uJ733yK4zEioi1FesNU'){
         return {
             key: script.chunks[3].buf.toString('utf-8'),
             value: script.chunks[4].buf.toString('utf-8'),
@@ -33,14 +33,14 @@ Protocol.resolveD = function (tx){
 Protocol.resolveB = function (tx){
     var tx = bsv.Transaction(tx)
     var Bscript = tx.outputs.filter(output=>output.script.isDataOut()||output.script.isSafeDataOut())[0].script
-    if(Bscript.chunks[1].buf == new Buffer('19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut')){
+    if(Bscript.chunks[1].buf && Bscript.chunks[1].buf.toString() === '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'){
         return {
             data: Bscript.chunks[2].buf,
             media_type: Bscript.chunks[3].buf.toString(),
             encoding: (Bscript.chunks[4])?Bscript.chunks[4].buf.toString():"",
             filename: (Bscript.chunks[5])?Bscript.chunks[5].buf.toString():""
         }    
-    } else if(Bscript.chunks[2].buf == new Buffer('19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut')){
+    } else if(Bscript.chunks[2].buf && Bscript.chunks[2].buf.toString() === '19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut'){
         return {
             data: Bscript.chunks[3].buf,
             media_type: Bscript.chunks[4].buf.toString(),
@@ -56,7 +56,7 @@ Protocol.resolveB = function (tx){
 Protocol.resolveBcat = function (tx){
     var tx = bsv.Transaction(tx)
     var Bscript = tx.outputs.filter(output=>output.script.isDataOut()||output.script.isSafeDataOut())[0].script
-    if(Bscript.chunks[1].buf == new Buffer('15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up')){
+    if(Bscript.chunks[1].buf && Bscript.chunks[1].buf.toString() === '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up'){
         return {
             info: Bscript.chunks[2].buf,
             media_type: Bscript.chunks[3].buf.toString(),
@@ -65,7 +65,7 @@ Protocol.resolveBcat = function (tx){
             flag: (Bscript.chunks[6])?Bscript.chunks[6].buf.toString():"",
             data:Bscript.chunks.slice(7).map(chunk=>chunk.buf.toString('hex'))
         }    
-    } else if(Bscript.chunks[2].buf == new Buffer('15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up')){
+    } else if(Bscript.chunks[2].buf && Bscript.chunks[2].buf.toString() === '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up'){
         return {
             info: Bscript.chunks[3].buf,
             media_type: Bscript.chunks[4].buf.toString(),
@@ -82,11 +82,11 @@ Protocol.resolveBcat = function (tx){
 Protocol.resolveBcatPart = function (tx){
     var tx = bsv.Transaction(tx)
     var Bscript = tx.outputs.filter(output=>output.script.isDataOut()||output.script.isSafeDataOut())[0].script
-    if(!(Bscript.chunks[1].buf == new Buffer('1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'))){
+    if(Bscript.chunks[1].buf && Bscript.chunks[1].buf.toString() === '1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'){
         return {
             data: Bscript.chunks[2].buf
         }    
-    } else if(!(Bscript.chunks[2].buf == new Buffer('1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'))){
+    } else if(Bscript.chunks[2].buf && Bscript.chunks[2].buf.toString() === '1ChDHzdd1H4wSjgGMHyndZm6qxEDGjqpJL'){
         return {
             data: Bscript.chunks[3].buf
         }    
